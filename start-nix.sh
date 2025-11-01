eae0dcd873a51eb3b2feac2e701fc5caf361567a
Kaur Kallas <kallas.kaur@gmail.com>
2025-11-01 18:15:01 +0000

    improved way to run nix in docker

--- start-nix.sh
+++ start-nix.sh
@@ -1,20 +1,19 @@
 # Just for testing, it makes sense to actually install nix on system lvl
 
-docker run -it --rm \
-  -v $(pwd):/workdir \
-  -v ~/.config/starship.toml:/root/.config/starship.toml:ro \
-  -w /workdir \
-  nixos/nix \
-  nix-shell
-
+# 1. create gitconfig (if using ssh auth)
+git config --global user.email "yourname@gmail.com"
+git config --global user.name "yourname"
 
 
 docker run -it --rm \
   -v $(pwd):/workdir \
   -v ~/.config/starship.toml:/root/.config/starship.toml:ro \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $HOME/.gitconfig:/root/.gitconfig:ro \
   -v $SSH_AUTH_SOCK:/ssh-agent \
   -e SSH_AUTH_SOCK=/ssh-agent \
   -w /workdir \
   nixos/nix \
   nix-shell
+
+
